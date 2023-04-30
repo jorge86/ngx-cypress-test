55be77fee1efbd05709c4f5a392f880dad4de01a
Jorge Desktop <george.mitrop@gmail.com>
2023-04-30 19:50:14 +0300

    change url

--- cypress/integration/examples/cypress/e2e/examples/inTest.cy.js
+++ cypress/integration/examples/cypress/e2e/examples/inTest.cy.js
@@ -1,25 +1,17 @@
-describe('in.gr search test', () => {
-    it('should display search results for "Selenium"', () => {
-      // Navigate to the in.gr website
-      cy.visit('https://www.in.gr');
-  
-      // Find the search input field, enter a search term, and submit the form
-      cy.get('#search')
-        .type('Selenium')
-        .type('{enter}');
-  
-      // Verify that search results are displayed
-      cy.get('.searchresults').should('be.visible');
-  
-      // Verify that search results contain the expected search term
-      const expectedTerm = 'Selenium';
-      cy.get('.searchresults h3 a').each(result => {
-        cy.wrap(result).should('contain', expectedTerm);
-      });
-  
-      // Verify that search results show the expected number of results
-      const expectedCount = 10;
-      cy.get('.searchresults .result').should('have.length', expectedCount);
-    });
-  }); 
-  
+describe('Google', () => {
+  it('should load the homepage and search', () => {
+    cy.visit('https://www.google.com')
+
+    // Verify that we landed on the Google homepage
+    cy.title().should('eq', 'Google')
+
+    // Type a search query and press Enter
+    cy.get('[name="q"]').type('Cypress.io{enter}')
+
+    // Verify that we landed on the search results page
+    cy.title().should('eq', 'Cypress.io - Google Search')
+
+    // Verify that there are search results displayed
+    cy.get('#search').should('be.visible')
+  })
+})
