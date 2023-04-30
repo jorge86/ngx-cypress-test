f61131b87d91635c04f17a92db9e22143d2a0444
Jorge Desktop <george.mitrop@gmail.com>
2023-04-30 21:18:35 +0300

    xpat

--- cypress/integration/examples/cypress/e2e/examples/inTest.cy.js
+++ cypress/integration/examples/cypress/e2e/examples/inTest.cy.js
@@ -1,7 +1,8 @@
 describe('Google Search', () => {
   it('should land on the homepage, accept cookies, make a search, and assert results', () => {
     cy.visit('https://www.google.com/');
-    cy.get('.QS5gu sy4vM').click();
+    //cy.get('.QS5gu sy4vM').click();
+    cy.xpath('/html[1]/body[1]/div[2]/div[2]/div[3]/span[1]/div[1]/div[1]/div[1]/div[3]/div[1]/button[2]/div[1]').click();
     cy.url().should('eq', 'https://www.google.com/');
     cy.get('input[name="q"]').type('cypress.io{enter}');
     cy.url().should('include', 'search');
